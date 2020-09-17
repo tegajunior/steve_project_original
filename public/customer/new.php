c4e81996dd3e233cfe3650cdf5223e37a72e58a7
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-09-17 12:46:15 +0100

    done with regstration page

--- public/customer/new.php
+++ public/customer/new.php
@@ -99,9 +99,9 @@
                             <p class="login-title2 text-center text-white">Let's setup your account real quick!</p>
                             <?php echo display_errors($errors); ?>
                         </div>
-                        <form onsubmit="loadingSpin()" action="<?php echo url_for('/customer/new.php'); ?>" method="post">
+                        <form onsubmit="return registerLoadingSpin()" action="<?php echo url_for('/customer/new.php'); ?>" method="post">
 
-                            <input type="text" name="first_name" placeholder="Firstname*" required autofocus>
+                            <!-- <input type="text" name="first_name" placeholder="Firstname*" required autofocus>
                             <input type="text" name="last_name" placeholder="Lastname*" required>
 
                             <select name="gender" id="gender">
@@ -110,19 +110,19 @@
                                 <option value="male">Female</option>
                             </select> 
 
-                            <input type="text" name="email" placeholder="Email" autocomplete="" required>
+                            <input type="text" name="email" placeholder="Email" autocomplete="" required> -->
 
                             <div class="position-relative">
                                 <i class="show-password fas fa-eye-slash position-absolute"></i>
-                                <input type="password" class="password" name="password" placeholder="Password" autocomplete="off" required>
+                                <input type="password" class="password" id="password-one" name="password" placeholder="Password" autocomplete="off" required>
                             </div>
 
                             <div class="position-relative">
                                 <i class="show-password fas fa-eye-slash position-absolute"></i>
-                                <input type="password" class="password" name="confirm_password" placeholder="confirm Password" autocomplete="off" required>
+                                <input type="password" class="password" id="password-two" name="confirm_password" placeholder="confirm Password" autocomplete="off" required>
                             </div>
                             
-                            <input type="date" name="date" placeholder="Date of Birth*" required>
+                            <!-- <input type="date" name="date" placeholder="Date of Birth*" required>
 
                             <input type="text" name="occupation" placeholder="Occupation" required>
 
@@ -132,7 +132,7 @@
 
                             <input type="text" name="address" placeholder="address" required>
 
-                            <input type="text" name="phone_number" placeholder="phone number" required>
+                            <input type="text" name="phone_number" placeholder="phone number" required> -->
 
                             <div class="text-center my-2">
                                 <span class="my-spin spinner-border spinner-border-sm text-success"></span>
