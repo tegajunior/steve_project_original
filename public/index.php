90f6be8c531fd6ea169fc30440125cb8bc8f6d4d
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-09-19 19:07:35 +0100

    added content to the landing page

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"><?php echo url_for('/public/customer/login.php') ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>United Arab Bank</title>
     <link rel="shortcut icon" href="<?php echo url_for('/images/favicon1.png') ?>" type="image/x-icon">
@@ -44,27 +44,26 @@
                 <div class="carousel-item active">
                     <img src="<?php echo url_for('/images/homepage_assets/download.jpg'); ?>" class="img-fluid" alt="Dubai Currency">
                     <div class="carousel-caption">
-                        <!-- <h3>Abu-dhabi Dubai</h3> -->
-                        <p>Experiencing good business</p>
+                        <h3>Experiencing good business</h3>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="<?php echo url_for('/images/homepage_assets/slide.jpg'); ?>" class="img-fluid" alt="Dubai Currency">
                     <div class="carousel-caption">
-                        <!-- <h3>Dubai</h3> -->
-                        <p>Investing in the future</p>
+                        <h3>Investing in the future</h3>
+                        <!-- <p>Investing in the future</p> -->
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="<?php echo url_for('/images/homepage_assets/slide-c.jpeg'); ?>" class="img-fluid" alt="Business Talk">
                     <div class="carousel-caption">
-                        <p>No limit to your cash</p>
+                        <h3>No limit to your cash</h3>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img src="<?php echo url_for('/images/homepage_assets/slide-d.png'); ?>" class="img-fluid" alt="Business Talk">
                     <div class="carousel-caption">
-                        <p>No limit to your cash</p>
+                        <h3>No limit to your cash</h3>
                     </div>
                 </div>
             </div>
@@ -88,22 +87,22 @@
              <div class="what-you-can-do row no-gutters justify-content-between">
                 <div class="col-md-3 p-2 my-2">
                     <i class="fas fa-users fa-5x d-block my-3"></i>
-                    <a href="">Manage all your accounts</a>
+                    <a href="<?php echo url_for('/customer/login.php'); ?>">Manage all your accounts</a>
                     <hr>
                 </div>
                 <div class="col-md-3 p-2 my-2">
                     <i class="fas fa-door-open fa-5x d-block my-3"></i>
-                    <a href="">Apply Online</a>
+                    <a href="<?php echo url_for('/customer/login.php'); ?>">Apply Online</a>
                     <hr>
                 </div>
                 <div class="col-md-3 p-2 my-2">
                     <i class="fas fa-chart-line fa-5x d-block my-3"></i>
-                    <a href="">Budget and Track</a>
+                    <a href="<?php echo url_for('/customer/login.php'); ?>">Budget and Track</a>
                     <hr>
                 </div>
                 <div class="col-md-3 p-2 my-2">
                     <i class="fas fa-people-carry fa-5x d-block my-3"></i>
-                    <a href="">Easy Withdraw</a>
+                    <a href="<?php echo url_for('/customer/login.php'); ?>">Easy Withdraw</a>
                     <hr>
                 </div>
             </div>
@@ -127,35 +126,88 @@
          <!--END of mobilebanking  section -->
 
          <!--About  section -->
-         <div class="about-section container py-3" id="about">
+         <div class="about-section container py-3 p-5 p-sm-0" id="about">
             <div class="header-wrapper my-2">
                 <h1 class="section-header text-center">About Us</h1>
                 <hr class="header-rule">
             </div>
-            <div class="row no-gutters align-items-center py-2">
+
+            <div class="row no-gutters align-items-center justify-content-between py-2">
                 <div class="col-md-3 text-center text-md-left my-3">
-                    <img src="<?php echo url_for('/images/homepage_assets/about-image-1.jpeg'); ?>" class="img-fluid">
+                    <img src="<?php echo url_for('/images/page_assets/uab_image_3.jpeg'); ?>" class="img-fluid rounded-circle">
                 </div>
-                <div class="col-md-9 px-2">
-                    <p>Commercial Bank of Dubai is a UAE banking and financial services corporation headquartered in Deira, Dubai. With more than $23 billion in assets, Gulf Business listed CBD as the 7th largest bank in the UAE, based on total assets. It also figures in the Dubai Financial Market index.</p>
-                </div>
-            </div>
-            <div class="row no-gutters align-items-center py-2">
-                <div class="col-md-3 text-center text-md-left my-3">
-                    <img src="<?php echo url_for('/images/homepage_assets/about-image-2.jpeg'); ?>" class="img-fluid">
-                </div>
-                <div class="col-md-9 px-2">
-                    <p>Established as a public institution with an independent legal entity by virtue of Decree 14/2000 issued by the Government of Dubai, the DFM lanched its activities on 26th March 2000.</p>
-                    <p>within a short period of time, the Dubai Finanical Market(DFM) has fast developed into a leading financial market across the region. its ongoing efforts and strategic initiatives have further reinforced Dubai as a centre of excellence in this part of the world and enhanced its leading postion as a powerful capital market hub which embraces international best practices to meet the evolving needs of its investors locally and internationally.</p>
+                <div class="col-md-8 px-2 about-description">
                     <p>DFM operates as a secondary market for the trading of securities issued by public shareholding companies, bonds issued by federal or local governments, local public institutions and mutual funds as wel as other local or foreign DFM approved financial instruments.</p>
                     <p>The Dubai Financial Market(DFM) is governed and regulated by the UAE Securities and Commodities Authority (SCA) which has the authority to impose laws and standards which the DFM must comply</p>
                 </div>
             </div>
+
+            <div class="row no-gutters align-items-center justify-content-between py-2">
+                <div class="col-md-3 text-center text-md-left my-3">
+                    <img src="<?php echo url_for('/images/page_assets/uab_image_1.jpeg'); ?>" class="img-fluid rounded-circle">
+                </div>
+                <div class="col-md-8 px-2 about-description">
+                    <p>Commercial Bank of Dubai is a UAE banking and financial services corporation headquartered in Deira, Dubai. With more than $23 billion in assets, Gulf Business listed CBD as the 7th largest bank in the UAE, based on total assets. It also figures in the Dubai Financial Market index.</p>
+                </div>
+            </div>
+
+            <div class="row no-gutters align-items-center justify-content-between py-2">
+                <div class="col-md-3 text-center text-md-left my-3">
+                    <img src="<?php echo url_for('/images/page_assets/uab_image_2.jpeg'); ?>" class="img-fluid rounded-circle">
+                </div>
+                <div class="col-md-8 px-2 about-description">
+                    <p>Established as a public institution with an independent legal entity by virtue of Decree 14/2000 issued by the Government of Dubai, the DFM lanched its activities on 26th March 2000.</p>
+                    <p>within a short period of time, the Dubai Finanical Market(DFM) has fast developed into a leading financial market across the region. its ongoing efforts and strategic initiatives have further reinforced Dubai as a centre of excellence in this part of the world and enhanced its leading postion as a powerful capital market hub which embraces international best practices to meet the evolving needs of its investors locally and internationally.</p>
+                </div>
+            </div>
+
          </div>
          <!--END of About  section -->
 
+         <!-- Features section -->
+        <div class="features row no-gutters align-items-center justify-content-between p-5">
+            <div class="features-aside col-md-4">
+                <h2 class="features-aside-header">FEATURED PRODUCTS & SERVICES</h2>
+                <p class="features-aside-para">Here are some of our hand-picked products & services that you may be interested in</p>
+            </div>
+            <div class="features-main col-md-7 d-flex flex-nowrap py-3">
+                <div class="features-main-content position-relative">
+                    <img class="features-main-img img-fluid" src="<?php echo url_for('/images/page_assets/uab_features_1.jpeg') ?>" alt="features images" >
+                    <div class="features-main-hidden position-absolute p-3">
+                        <h3 class="feature-header">Islamic Credit Card</h3>
+                        <p class="feature-header">The UAB Islamic credit card enables you to manage your finances effectively. With worldwide acceptance at millions of outlets, our Islamic cards offer a host of features and benefits that make your life simpler and more convenient.</p>
+                    </div>
+                </div>
+
+                <div class="features-main-content position-relative mx-2">
+                    <img class="features-main-img img-fluid" src="<?php echo url_for('/images/page_assets/uab_features_4.jpg') ?>" alt="features images" >
+                    <div class="features-main-hidden position-absolute p-3">
+                        <h3 class="feature-header">All Credit Cards</h3>
+                        <p class="feature-header">All Credit Cards is your key to a world of benefits.</p>
+                    </div>
+                </div>
+
+                <div class="features-main-content position-relative mx-2">
+                    <img class="features-main-img img-fluid" src="<?php echo url_for('/images/page_assets/uab_features_3.jpeg') ?>" alt="features images" >
+                    <div class="features-main-hidden position-absolute p-3">
+                        <h3 class="feature-header">Personal Loan</h3>
+                        <p class="feature-header">Since your every aspiration matters to us, UAB offers personal loans with flexible repayment terms under customised banking solution to meet your individual needs.</p>
+                    </div>
+                </div>
+
+                <div class="features-main-content position-relative">
+                    <img class="features-main-img img-fluid" src="<?php echo url_for('/images/page_assets/uab_features_2.jpeg') ?>" alt="features images" >
+                    <div class="features-main-hidden position-absolute p-3">
+                        <h3 class="feature-header">Raha call Deposit</h3>
+                        <p class="feature-header">A first of its kind Call Deposit account with exciting returns, hassle-free and fully accessible deposits & unlimited withdrawals.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!-- END of Features section -->
+
          <!-- contact us section -->
-         <div class="font-family: 'Roboto', sans-serif;font-family: 'Roboto', sans-serif; container py-3" id="contact">
+         <div class="contact container-fluid py-3 p-5" id="contact">
             <div class="header-wrapper my-2">
                 <h1 class="section-header text-center">Contact Us</h1>
                 <hr class="header-rule">
